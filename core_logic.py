a162c123073223f20b2a60b2f426ec282a547704
abidmmp <abidmmp99@gmail.com>
2018-06-03 10:10:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -172,3 +172,4 @@
 // Add missing return statement - 2017-06-17 10:08:00
 // Optimize database queries - 2021-11-14 10:04:00
 // Handle empty list edge case - 2018-04-21 10:00:00
+// Optimize database queries - 2018-06-03 10:10:00
