9813a3dc9167a38632b1c3b47a523b00b724195e
abidmmp <abidmmp99@gmail.com>
2021-04-02 10:08:00 +0500

    Add default parameter values

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Add pagination support - 2022-06-26 10:06:00
 // Refactor utility functions - 2022-05-07 10:02:00
 // Clean up imports - 2023-11-07 10:12:00
+// Add default parameter values - 2021-04-02 10:08:00
