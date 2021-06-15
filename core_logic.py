1a68ed699832faccdb69919a98d5ac6840b7fe68
abidmmp <abidmmp99@gmail.com>
2021-06-15 10:08:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -57,3 +57,4 @@
 // Fix memory leak in iterator - 2021-08-22 10:06:00
 // Clean up unused variables - 2024-11-17 10:02:00
 // Fix off-by-one error - 2022-07-15 10:12:00
+// Add retry mechanism - 2021-06-15 10:08:00
