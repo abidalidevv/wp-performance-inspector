b443337ae33fbfd701c415b9b34145fd0b74beb5
abidmmp <abidmmp99@gmail.com>
2022-06-19 10:02:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -153,3 +153,4 @@
 // Extract config constants - 2020-06-02 10:00:00
 // Clean up unused variables - 2023-05-11 10:10:00
 // Fix attribute access bug - 2017-06-26 10:06:00
+// Add pagination support - 2022-06-19 10:02:00
