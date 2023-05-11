6be35f7eda49b24588311293a70e81a069454ccc
abidmmp <abidmmp99@gmail.com>
2023-05-11 10:10:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -151,3 +151,4 @@
 // Optimize database queries - 2019-07-14 10:08:00
 // Optimize loop performance - 2021-07-03 10:04:00
 // Extract config constants - 2020-06-02 10:00:00
+// Clean up unused variables - 2023-05-11 10:10:00
