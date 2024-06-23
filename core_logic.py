f716eeda9766dfa4150a7b36361a0ef9dc56c7b7
abidmmp <abidmmp99@gmail.com>
2024-06-23 10:06:00 +0500

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -68,3 +68,4 @@
 // Improve exception messages - 2021-11-06 10:04:00
 // Fix off-by-one error - 2021-10-13 10:00:00
 // Handle None edge case - 2021-07-07 10:10:00
+// Handle empty list edge case - 2024-06-23 10:06:00
