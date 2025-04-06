857ef5a4d0d6e094dc5782591768ffd4aa475b64
Abid Ali <abidmmp99@gmail.com>
2025-04-06 09:31:10 +0000

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -298,3 +298,7 @@
 def chunk_list(lst, size):
     for i in range(0, len(lst), size):
         yield lst[i:i+size]
+
+def chunk_list(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
