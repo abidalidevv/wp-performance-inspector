579485743c8ba6e97f2f638378d87bb3edea5390
Abid Ali <abidmmp99@gmail.com>
2025-04-19 11:22:46 +0000

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -416,3 +416,9 @@ def chunk_list(lst, size):
 
 def format_date(dt):
     return dt.strftime('%Y-%m-%d')
+
+def human_size(n):
+    for u in ['B','KB','MB','GB']:
+        if n < 1024: return f'{n:.1f} {u}'
+        n /= 1024
+    return f'{n:.1f} TB'
